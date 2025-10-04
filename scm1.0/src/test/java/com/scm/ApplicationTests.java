c3f753b3d4cb85fb74b291ccede4a6243c00a301
Tetrax <dipeshtiwari47@gmail.com>
2025-10-05 02:04:46 +0530

    Implement Mailtrap email service

--- scm1.0/src/test/java/com/scm/ApplicationTests.java
+++ scm1.0/src/test/java/com/scm/ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.scm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scm.services.EmailService;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private EmailService emailService;
+
+	@Test
+	void sendEmailTest()
+	{
+		emailService.sendEmail("faltu111999@gmail.com", "Test email for ccm", "Testing email sending service from mailtrap");
 	}
 
 }
